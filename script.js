78a40bcb689cac1dc1fa48e7058497b172971d89
zou2023 <mikezourntos@gmail.com>
2023-01-09 18:42:27 +0200

    edit

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function getComputerChoice() {
         return "scissors"
     }
 }
+let playerdisp = document.getElementById('playerscore')
+let compdisp = document.getElementById('computerscore')
+let rock = document.getElementById('r');
+let paper = document.getElementById('p');
+let scissors = document.getElementById('s');
 
 let computerscore = 0
 let playerscore = 0
@@ -21,26 +26,53 @@ function playRound(player, computer) {
         return "computer wins"
     } else if (player == computer) {
         return "draw"
-    }
+    } 
     
 }
 
+rock.addEventListener('click', () => {
+    const computerchoice = getComputerChoice();
+    console.log(playRound("rock", computerchoice))
+    playerdisp.textContent = playerscore;
+    compdisp.textContent = computerscore; 
+})
+
+paper.addEventListener('click', () => {
+    const computerchoice = getComputerChoice();
+    console.log(playRound("paper", computerchoice))
+    playerdisp.textContent = playerscore;
+    compdisp.textContent = computerscore; 
+})
+
+scissors.addEventListener('click', () => {
+    const computerchoice = getComputerChoice();
+    console.log(playRound("scissors", computerchoice))
+    playerdisp.textContent = playerscore;
+    compdisp.textContent = computerscore; 
+})
 
 
 
-function game() {
+function check() {
+    if (playerscore > computerscore){
+        console.log('You Win')
+    } else if (playerscore < computerscore) {
+        console.log('Computer Wins')
+    } else {console.log('Draw')}
+}
+
+/*function game() {
     
     for (let i = 0; i < 5; i++) {
-        const computerchoice = getComputerChoice();
         const playerchoice = prompt("Rock, Paper or Scissors?").toLowerCase();
         console.log(playerchoice)
         console.log(playRound(playerchoice, computerchoice))
         console.log("PLAYER " + playerscore + " - " + "COMPUTER " + computerscore) 
    }
-   console.log("end")
+   check()
 }
 
-game()
+game()*/
 
 
 
